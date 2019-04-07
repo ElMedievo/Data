f3c8bb7fceff22e2c6c6360b58e5f5c7c37bb0c4
Jorge Orlando <bgmp00@protonmail.com>
2019-04-06 22:54:56 -0300

    Increase dedicated RAM for the server

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 while true
 do
-java -Xms1024M -Xmx1024M -Djline.terminal=jline.UnsupportedTerminal -jar Spigot.jar
+java -Xms2024M -Xmx2024M -Djline.terminal=jline.UnsupportedTerminal -jar Spigot.jar
 echo "Server restarting in 5 seconds..."
 sleep 5
 done
