415cd89a53ed070fc4bb0c7c11725f9857b51ff1
Jorge Orlando <bgmp00@protonmail.com>
2019-06-10 02:10:56 -0400

    Decrease server RAM

--- Server/restart.sh
+++ Server/restart.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 while true
 do
-java -Xms1900M -Xmx1900M -Djline.terminal=jline.UnsupportedTerminal -jar Spigot.jar
+java -Xms1600M -Xmx1600M -Djline.terminal=jline.UnsupportedTerminal -jar Spigot.jar
 echo "Server restarting in 5 seconds..."
 sleep 5
 done
