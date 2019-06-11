c2232c14deeeb74dcf9ce699f69792a62bb6c0a1
Jorge Orlando <bgmp00@protonmail.com>
2019-06-11 00:07:37 -0400

    Tweak JVM params

--- Server/restart.sh
+++ Server/restart.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 while true
 do
-java -Xms1600M -Xmx1600M -Djline.terminal=jline.UnsupportedTerminal -jar Spigot.jar
+java -Xms1400M -Xmx1900M -Djline.terminal=jline.UnsupportedTerminal -jar Spigot.jar
 echo "Server restarting in 5 seconds..."
 sleep 5
 done
