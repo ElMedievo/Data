6901a0223a28d95fdbec21fa3fa6809d6a5f612d
Jorge Orlando <bgmp00@protonmail.com>
2019-06-29 02:42:56 -0400

    Bump server RAM

--- Server/restart.sh
+++ Server/restart.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 while true
 do
-java -Xms1400M -Xmx1900M -Djline.terminal=jline.UnsupportedTerminal -jar Spigot.jar
+java -Xms2000M -Xmx2500M -Djline.terminal=jline.UnsupportedTerminal -jar Spigot.jar
 echo "Server restarting in 5 seconds..."
 sleep 5
 done
