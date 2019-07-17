ca9061ab4360d452f6bee7e56683af13c713c9dc
Jorge Orlando <bgmp00@protonmail.com>
2019-07-17 01:52:17 -0400

    Switch to Paper

--- Server/restart.sh
+++ Server/restart.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 while true
 do
-java -Xms2000M -Xmx2500M -Djline.terminal=jline.UnsupportedTerminal -jar Spigot-1.14.3.jar
+java -Xms2500M -Xmx3000M -Djline.terminal=jline.UnsupportedTerminal -jar Paper-1.14.3.jar
 echo "Server restarting in 5 seconds..."
 sleep 5
 done
