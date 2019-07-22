d568a57c7992dfeb83608f12a828a57a14565466
BGM <bgmp00@protonmail.com>
2019-07-22 01:54:50 -0400

    Update server restart script

--- Server/restart.sh
+++ Server/restart.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 while true
 do
-java -Xms2500M -Xmx3000M -Djline.terminal=jline.UnsupportedTerminal -jar Paper-1.14.3.jar
+java -Xms2500M -Xmx3000M -Djline.terminal=jline.UnsupportedTerminal -jar Paper-1.14.2.jar
 echo "Server restarting in 5 seconds..."
 sleep 5
 done
