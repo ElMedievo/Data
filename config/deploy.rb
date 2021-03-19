214732f056f3a5ef8904324ff429873fde9cdffb
José Benavente <jose@bgmp.cl>
2021-03-19 17:03:48 -0300

    Remove unnecessary mounting task

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,13 +17,6 @@ namespace :app do
   task :update_rvm_key do
     execute :gpg, "--keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
   end
-
-  #task :mount do
-  #  on roles(:app) do
-  #    execute ""
-  #  end
-  #end
 end
 
 before "rvm1:install:rvm", "app:update_rvm_key"
-#after "deploy", "app:mount"
