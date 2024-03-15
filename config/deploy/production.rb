f483b52428a2df0207f9f601b32fae756bd6a4b3
BGMP <jose@bgm.cl>
2024-03-15 00:18:03 -0300

    Remove unnecessary config file

--- config/deploy/production.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-server 'network.bgm.cl', :user => 'deploy', :roles => 'app', :primary => true
-
-set :ssh_options, {
-  :keys => %w(C:/Users/BGM/.ssh/elmedievo),
-  :auth_methods => %w(publickey),
-  :forward_agent => true
-}
